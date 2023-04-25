454faa3ee36ef95d6aedbe8add98b5fbdb1381c8
jianjian <zhaojianxfs@xinfushe.com>
2023-04-25 15:56:12 +0800

    add webpack和babel类型提示

--- docs/config/sidebar.ts
+++ docs/config/sidebar.ts
@@ -7,7 +7,7 @@ const sidebar: DefaultTheme.Sidebar = {
       items: [
         { text: 'webpack的基本使用', link: '/webpack/baseUse' },
         { text: 'webpack的打包结果分析', link: '/webpack/buildAnalysis' },
-        { text: 'webpack和babel配置文件类型提示', link: '/webpack/typeTips' },
+        { text: '配置文件如何能有类型提示', link: '/webpack/typeTips' },
       ],
     },
   ],
