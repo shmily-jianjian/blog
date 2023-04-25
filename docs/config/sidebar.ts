468aa3e3aee1d12ff224d775891a6907a5e00603
jianjian <zhaojianxfs@xinfushe.com>
2023-04-25 09:22:56 +0800

    feat: add build分析

--- docs/config/sidebar.ts
+++ docs/config/sidebar.ts
@@ -4,7 +4,10 @@ const sidebar: DefaultTheme.Sidebar = {
   '/webpack': [
     {
       text: 'webpack',
-      items: [{ text: 'webpack的基本使用', link: '/webpack/baseUse' }],
+      items: [
+        { text: 'webpack的基本使用', link: '/webpack/baseUse' },
+        { text: 'webpack的打包结果分析', link: '/webpack/buildAnalysis' },
+      ],
     },
   ],
   '/babel': [
