9080279aadf65bff35ecc73e40a698950c75031d
jianjian <zhaojianxfs@xinfushe.com>
2023-04-27 14:18:28 +0800

    feat: add module-chunk-bundle区别

--- docs/config/sidebar.ts
+++ docs/config/sidebar.ts
@@ -9,6 +9,7 @@ const sidebar: DefaultTheme.Sidebar = {
         { text: 'webpack的打包结果分析', link: '/webpack/buildAnalysis' },
         { text: '配置文件如何能有类型提示', link: '/webpack/typeTips' },
         { text: 'webpack优化分析', link: '/webpack/webpackOptimize' },
+        { text: 'module,chunk.bundle区别', link: '/webpack/module-chunk-bundle' },
       ],
     },
   ],
